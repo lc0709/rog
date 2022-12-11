2398eb56fd2995f51d2f4a9107a490a54eed31be
lc0709 <3dollee@gmail.com>
2022-12-11 16:24:17 +0900

    block(session 3)!

--- src/main/java/kr/doiche/rog/block/RogBlocks.java
+++ src/main/java/kr/doiche/rog/block/RogBlocks.java
@@ -6,10 +6,12 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.block.OreBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.minecraft.util.registry.Registry;
 
 public class RogBlocks {
@@ -17,7 +19,25 @@ public class RogBlocks {
             "rog_block",
             new Block(FabricBlockSettings.of(Material.METAL).strength(4f).luminance(2).requiresTool()),
             RogItemgroups.ROG
-            );
+    );
+
+    public static final Block ROG_ORE_BLOCK = registerBlock(
+            "rog_ore_block",
+            new OreBlock(
+                    FabricBlockSettings.of(Material.STONE).strength(4f).luminance(2).requiresTool(),
+                    UniformIntProvider.create(3, 7)
+            ),
+            RogItemgroups.ROG
+    );
+
+    public static final Block DEEPSLATE_ROG_ORE_BLOCK = registerBlock(
+            "deepslate_rog_ore_block",
+            new OreBlock(
+                    FabricBlockSettings.of(Material.STONE).strength(4f).luminance(2).requiresTool(),
+                    UniformIntProvider.create(3, 7)
+            ),
+            RogItemgroups.ROG
+    );
 
     private static Block registerBlock(String name, Block block, ItemGroup tab){
         registerBlockItem(name, block, tab);
